bc39b27b3a7077cd76621d44c83fbc6e0a7a28da
Alexandr Larionov <alarionov93@yandex.ru>
2018-11-01 00:47:19 +0500

    created more models and tests

--- core/models.py
+++ core/models.py
@@ -1,30 +1,82 @@
 from django.db import models
+from datetime import datetime, timedelta
+import sys
 
 # Create your models here.
 
 class JournalRecord(models.Model):
-	master = models.ForeignKey(to='Master')
-	time_elapsed = 
+	master = models.ForeignKey('Master', to_field='id', db_column='master_id', blank=False, null=False, unique=False,
+                                verbose_name='Назначенный мастер', on_delete=models.CASCADE)
+	service = models.ForeignKey('Service', to_field='id', db_column='service_id', blank=False, null=False, unique=False,
+                                verbose_name='Услуга', on_delete=models.CASCADE)
+	time = models.CharField(max_length=100, default='')
+	client = models.CharField(max_length=100, default='')
 
-	def save():
+	@property
+	def duration(self):
+		return self.service.time_elapsed
+
+	def save(self, *args, **kwargs):
+		print(self.service.time_elapsed, file=sys.stderr)
+		if self.master.__check_time__(self.time):
+			print('Created')
+		else:
+			raise Exception('Wrong time values!')
+
+
+		super(JournalRecord, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		db_table = 'journal_record'
+		verbose_name = 'Запись'
+		verbose_name_plural = 'Записи'
 
 
 class Master(models.Model):
 	name = models.CharField(max_length=100, blank=False, null=False, unique=True, verbose_name='ФИО мастера')
-	vacant_days = models.CommaSeparatedIntegerField(default='6,7')
+	vacant_days = models.CharField(max_length=50, default='6,7')
 	vacant_hours = models.CharField(max_length=100, default='18-9, 13-14')
+	# where to store occupied times and dates of masters????
+	# 
+
+	@property
+	def time_table(self):
+		v_hours_intervals = [ _.strip() for _ in self.vacant_hours.split(',')]
+		v_hours_splitted = [ (int(x)-1, int(y)) for x,y in [h.split('-') for h in v_hours_intervals] ]
+		vh = []
+
+		for h in range(0,24):
+			for a,b in v_hours_splitted:
+				if a > b:
+					if 0 <= h < b or a < h < 24:
+						vh += [h]
+				else:
+					if a < h < b:
+						vh += [h]
+
+		return vh
 
 	def __check_time__(self, asked_time):
-		checked = 0
-		if asked_time != vacant_days: # temporary
-			checked = 1
-		return checked
+		# for test only !
+		asked = datetime.strptime(asked_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+		if not (asked.weekday() in [int(x) for x in self.vacant_days.split(',')]) and \
+			not (asked.hour) in self.time_table:
+			print('Free', file=sys.stderr)
+			return True
+
+		return False
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		db_table = 'master'
+		verbose_name = 'Мастер'
+		verbose_name_plural = 'Мастера'
 
 
 class Service(models.Model):
@@ -34,4 +86,9 @@ class Service(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		db_table = 'service'
+		verbose_name = 'Услуга'
+		verbose_name_plural = 'Услуги'
 
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from core.models import *
+import sys
 # Create your tests here.
+
+class TestJournalRec(TestCase):
+	def setUp(self):
+		anton = Master.objects.create(name='Anton', vacant_days='6,7', vacant_hours='18-9, 13-15')
+		shpilling = Service.objects.create(name='Шпилинг', price=100500, time_elapsed=2)
+		rec0 = JournalRecord.objects.create(master=anton, service=shpilling, time='2018-11-04T17:00:00.000Z', client='Lena')
+		rec1 = JournalRecord.objects.create(master=anton, service=shpilling, time='2018-11-04T17:00:00.000Z', client='Katya')
+		rec2 = JournalRecord.objects.create(master=anton, service=shpilling, time='2018-11-04T14:00:00.000Z', client='Vasya')
+
+	def test(self):
+		print(JournalRecord.objects.all(), file=sys.stderr)
+
+		return 0
+        
